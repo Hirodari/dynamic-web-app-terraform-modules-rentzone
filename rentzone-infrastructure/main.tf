a697e9ffba61d21008b91bd92e39a38c598f17c6
Fred B <fredbitenyo@gmail.com>
2023-06-07 00:15:46 +0200

    adding database

--- rentzone-infrastructure/main.tf
+++ rentzone-infrastructure/main.tf
@@ -38,10 +38,26 @@ module "natgateway" {
 
 # create Security Group
 
-module "natgateway" {
-  source                     = "git@github.com:Hirodari/dynamic-web-app-terraform-modules-rentzone.git//rentzone-modules/security-group"
+module "security-group" {
+  source       = "git@github.com:Hirodari/dynamic-web-app-terraform-modules-rentzone.git//rentzone-modules/security-group"
   project_name = local.project_name
-  environment = local.environment
-  vpc_id = module.vpc.vpc_id
-  ssh_ip = var.ssh_ip
+  environment  = local.environment
+  vpc_id       = module.vpc.vpc_id
+  ssh_ip       = var.ssh_ip
+}
+
+# create RDS
+
+module "database" {
+  source                     = "git@github.com:Hirodari/dynamic-web-app-terraform-modules-rentzone.git//rentzone-modules/rds"
+  project_name               = local.project_name
+  environment                = local.environment
+  private_data_subnet_az1_id = module.vpc.private_data_subnet_az1_id
+  private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
+  db_snapshot_identifier     = var.db_snapshot_identifier
+  db_instance_class          = var.db_instance_class
+  availability_zone_1        = module.vpc.availability_zone_1
+  db_instance_identifier     = var.db_instance_identifier
+  multi_az_deployment        = var.multi_az_deployment
+  database_sg_id             = module.security-group.database_sg_id
 }
