959c23ec997ca857791901b132194602d315ce9a
Fred B <fredbitenyo@gmail.com>
2023-06-09 18:29:11 +0200

    correcting ec2 module

--- rentzone-modules/ec2/main.tf
+++ rentzone-modules/ec2/main.tf
@@ -17,41 +17,62 @@ data "aws_ami" "amazon_linux_2" {
 
 # launch the ec2 instance and install website
 resource "aws_instance" "ec2_instance_az1" {
-  ami                    = data.aws_ami.amazon_linux_2
+  ami                    = data.aws_ami.amazon_linux_2.id # need to be replaced by this id: ami-09988af04120b3591
   instance_type          = var.instance_type
   subnet_id              = var.public_subnet_az1_id
-  vpc_security_group_ids = var.application_sg_id
+  vpc_security_group_ids = [var.application_sg_id]
   key_name               = var.key_name
   user_data              = file("conf/install_rentzone.sh")
 
   tags = {
-    Name = "${var.project_name} | Rentzone Web App"
+    Name = "${var.project_name} | Rentzone Web App AZ1"
+  }
+}
+
+resource "null_resource" "file_transport" {
+  provisioner "file" {
+    source      = "/home/hiro/Documents/AOS_docs/dynamic-web-app-terraform-modules-rentzone/rentzone-infrastructure/conf/install_rentzone.sh"
+    destination = "/home/ec2-user/install_rentzone.sh"
+  }
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = "${file("/home/hiro/Documents/AOS_docs/dynamic-ecommerce/hiro_kp.pem")}"
+    host        = aws_instance.ec2_instance_az1.public_dns
   }
 }
 
 # launch the ec2 instance and install website
-resource "aws_instance" "ec2_instance_az2" {
-  ami                    = data.aws_ami.amazon_linux_2
-  instance_type          = var.instance_type
-  subnet_id              = var.public_subnet_az2_id
-  vpc_security_group_ids = [var.application_sg_id]
-  key_name               = var.key_name
-  user_data              = file("conf/install_rentzone.sh")
+# resource "aws_instance" "ec2_instance_az2" {
+#   ami                    = data.aws_ami.amazon_linux_2.id
+#   instance_type          = var.instance_type
+#   subnet_id              = var.public_subnet_az2_id
+#   vpc_security_group_ids = [var.application_sg_id]
+#   key_name               = var.key_name
+#   user_data              = file("conf/install_rentzone.sh")
   
 
-  tags = {
-    Name = "${var.project_name} | Rentzone Web App"
-  }
+#   tags = {
+#     Name = "${var.project_name} | Rentzone Web App AZ2"
+#   }
 
-  # Copies the conf files file to /home/ec2-user
-  provisioner "file" {
-    source      = "conf/install_rentzone.sh"
-    destination = "/home/ec2-user"
-  }
-}
+#   connection {
+#     type = "ssh"
+#     user        = "ec2-user"
+#     private_key = "${file("/home/hiro/Documents/AOS_docs/dynamic-ecommerce/hiro_kp.pem")}"
+#     host = "${self.public_ip}"
+#   } 
+
+#   # Copies the conf files file to /home/ec2-user
+#   provisioner "file" {
+#     source      = "/home/hiro/Documents/AOS_docs/dynamic-web-app-terraform-modules-rentzone/rentzone-infrastructure/conf/install_rentzone.sh"
+#     destination = "/home/ec2-user/install_rentzone.sh"
+#   }
+# }
 
 
 
 # print the ec2's public ipv4 address
 output "public_ipv4_address" {
-  value = aws_instance.ec2_instance_az1.public_ipv4_address
+  value = aws_instance.ec2_instance_az1.public_ip
+}
