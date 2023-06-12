55bc2cb5defd517e989cf628fd319647b4ba61eb
Fred B <fredbitenyo@gmail.com>
2023-06-12 12:50:58 +0200

    adding load balancer and target group

--- rentzone-infrastructure/main.tf
+++ rentzone-infrastructure/main.tf
@@ -75,7 +75,7 @@ module "ec2" {
 }
 
 # create Load Balancer
-module "ec2" {
+module "alg" {
   source               = "git@github.com:Hirodari/dynamic-web-app-terraform-modules-rentzone.git//rentzone-modules/alb"
   project_name         = local.project_name
   environment = local.environment
